b0fbdb80357a61ed4620294ab342ab167fa5e8c1
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-07-07 14:05:00 +0100

    :construction: arrays

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -4,11 +4,6 @@ pub fn main() void {
     std.debug.print("Hello, {s}!\n", .{"World"});
 }
 
-const constant: i32 = 5;
-var variable: u32 = 5000;
-
-const inferred_constant = @as(i32, 5);
-var inferred_variable = @as(u32, 5000);
-
-const a: i32 = undefined;
-var b: u32 = undefined;
+const a = [5]u8{ 'h', 'e', 'l', 'l', 'o' };
+const b = [_]u8{ 'w', 'o', 'r', 'l', 'd' };
+const length = b.len;
