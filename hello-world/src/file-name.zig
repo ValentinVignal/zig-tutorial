a10c9a2044603bb3d9ae9b45fd15344df3675042
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-07-10 17:42:20 +0800

    :construction: floats

--- hello-world/src/file-name.zig
+++ hello-world/src/file-name.zig
@@ -1,15 +1,8 @@
 const expect = @import("std").testing.expect;
 
-const Tag = enum { a, b, c };
-
-const Tagged = union(Tag) { a: u8, b: f32, c: bool };
-
-test "switch on tagged union" {
-    var value = Tagged{ .b = 1.5 };
-    switch (value) {
-        .a => |*byte| byte.* += 1,
-        .b => |*float| float.* *= 2,
-        .c => |*b| b.* = !b.*,
-    }
-    try expect(value.b == 3);
+test "int-float conversion" {
+    const a: i32 = 0;
+    const b = @as(f32, @floatFromInt(a));
+    const c = @as(i32, @intFromFloat(b));
+    try expect(c == a);
 }
